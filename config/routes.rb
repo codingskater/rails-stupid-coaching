3517902ba322515fa5e9bcfb702774475f2093bc
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-09-14 22:44:21 +0900

    added a proper home page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root "questions#index"
   get "ask", to: "questions#ask"
   get "answer", to: "questions#answer"
 end
