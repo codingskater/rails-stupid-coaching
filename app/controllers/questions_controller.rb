d27824d2a15ca1b5b12aa113345036080023904f
Anna Costello <271207945+codingskater@users.noreply.github.com>
2026-09-18 13:48:21 +0900

    updated code for different presentation of user & coach exchanges

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,12 +3,23 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params["question"] == "I am going to work"
+    question = params["question"].to_s
+
+    if question == "I am going to work"
       @answer = "Great!"
-    elsif params["question"][-1] == "?"
+    elsif question.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
       @answer = "I don't care, get dressed and go to work!"
+    end
+
+    @conversation = session[:conversation] || []
+    @conversation << { "question" => question, "answer" => @answer }
+
+    if @answer == "Great!"
+      session.delete(:conversation)
+    else
+      session[:conversation] = @conversation
     end
   end
 end
--- test/controllers/questions_controller_test.rb
+++ test/controllers/questions_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class QuestionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "keeps the previous chat until the answer is Great" do
+    get answer_path, params: { question: "Can I take a break?" }
+    assert_response :success
+
+    get answer_path, params: { question: "What about tomorrow?" }
+    assert_response :success
+    assert_includes response.body, "Can I take a break?"
+    assert_includes response.body, "What about tomorrow?"
+    assert_includes response.body, "Ask!"
+
+    get answer_path, params: { question: "I am going to work" }
+    assert_response :success
+    assert_includes response.body, "Can I take a break?"
+    assert_includes response.body, "What about tomorrow?"
+    assert_includes response.body, "Great!"
+    refute_includes response.body, 'name="question"'
+    assert_includes response.body, "Consult Again"
+  end
 end
